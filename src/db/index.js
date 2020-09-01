c48ce818ec88d79250296a27f41af96088c61665
jeffreycao1998 <jeffreycao1998@hotmail.com>
2020-09-01 09:49:54 -0400

    added server api with connected db

--- src/db/index.js
+++ src/db/index.js
@@ -1,7 +1,12 @@
+require('dotenv').config();
 const pg = require("pg");
 
 const client = new pg.Client({
-  connectionString: process.env.DATABASE_URL || ""
+  host: process.env.PGHOST,
+  port: process.env.PGPORT,
+  user: process.env.PGUSER,
+  password: process.env.PGPASSWORD,
+  database: process.env.PGDATABASE,
 });
 
 client
